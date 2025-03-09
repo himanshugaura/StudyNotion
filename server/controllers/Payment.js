d3fdeaab2df3f7e8c9f0511c8c3b039c1dd45914
himanshu <himanshu32505@gmail.com>
2025-03-09 13:31:50 +0530

    updated : course & payment contoller

--- server/controllers/Payment.js
+++ server/controllers/Payment.js
@@ -5,48 +5,48 @@ const mailSender = require("../utils/mailSender");
 const {courseEnrollmentEmail} = require("../mailTemplate/courseEnrollmentEmail");
 const { mongoose } = require("mongoose");
 
-exports.capturePayment = async (req , res) =>
+exports.capturePayment  = async (req , res) =>
 {
     const {course_id} = req.body;
-
     const userId = req.user.id;
 
     if(!course_id)
     {
         return res.json({
             success : false,
-            message : "Please provide vaild course ID"
+            message : "please provide valid course Id"
         })
+        
     }
 
     let course;
-
     try {
         course = await Course.findById(course_id);
         if(!course)
-            {
-                return res.json({
-                    success : false,
-                    message : "unable to find the course"
-                })
-            }
+        {
+            return res.json({
+                success : false,
+                message :"Could not find the course"
+            })
+        }
 
-            const uid = new mongoose.Types.ObjectId(userId);
-            if(course.studentEnrolled.includes(uid))
-            {
-                return res.status(200).json({
-                    success : false,
-                    message : "Student is already enrolled"
-                })
-            }
+        const uid = new mongoose.Types.ObjectId(userId);
 
+        if(course.studentEnrolled.includes(uid))
+        {
+            return res.json({
+                success : false,
+                message : "student is already enrolled"
+            })
+        }
     } catch (error) {
         console.error(error);
         return res.status(500).json({
-            success : false ,
-            message : error.message,
+            success : false,
+            message : error.message
         })
     }
+
 
     const amount = course.price;
     const currency = "INR";
@@ -54,13 +54,13 @@ exports.capturePayment = async (req , res) =>
     const options = {
         amount : amount * 100,
         currency : currency,
-        receipt : Math.random(Date.now()).toString(),
-        notes:{
-            courseId : course._id,
+        receipt : Math.random(Date.now()).toString,
+        notes : 
+        {
+            courseId : course_id,
             userId,
         }
-    };
-
+    }
 
     try {
         const paymentResponse = await instance.orders.create(options);
@@ -70,26 +70,17 @@ exports.capturePayment = async (req , res) =>
             success : true,
             courseName : course.courseName,
             courseDescription : course.courseDescription,
-            courseThumbnail : course.thumbnail,
+            thumbnail = course.thumbnail,
             orderId : paymentResponse.id,
             currency : paymentResponse.currency,
             amount : paymentResponse.amount,
         })
+        
     } catch (error) {
         console.log(error);
-        return res.json({
+        res.json({
             success : false,
-            message : "could not initiate order"
+            message : "could no initiate order"
         })
     }
 }
-
-
-exports.verifySignature = async (req , res) =>
-{
-    const webhookSecret = "12345678";
-
-    const signature = req.header("x-razorpay-signature");
-
-    crypto.createHmac
-};
